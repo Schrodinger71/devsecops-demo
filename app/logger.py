4105b75c0fb68985c949dd6ca4578fe8b087b3ef
Schrodinger71 <sanya.podshibyakin@mail.ru>
2026-01-12 16:46:02 +0300

    SAST уязвимость: инъекция команд через subprocess

--- app/logger.py
+++ app/logger.py
@@ -1,7 +1,13 @@
 import logging
+import subprocess  # опасный импорт
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s"
 )
 
 logger = logging.getLogger("devsecops-app")
+
+# Опасная функция для демонстрации Bandit
+def execute_command(user_input):
+    # Уязвимость к инъекции команд
+    subprocess.call(f"echo {user_input}", shell=True)  # B602
